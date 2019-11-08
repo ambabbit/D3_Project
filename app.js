d05a27fd381bc76d9f0fe7e1d641bf08ed39c56c
Pikachubolt <kingtaym@gmail.com>
2019-11-07 19:20:21 -0500

    more button actions

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 function openModel1() { 
     window.open ("https://www.google.com", "_blank");
-    }
+}
+
+function openModel2() {
+    window.open ("https://www.google.com", "_blank");
+}
+
+function openModel3() {
+    window.open ("https://www.google.com", "_blank");
+}
