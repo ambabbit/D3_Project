b70e495fb8d49469a414fac74ca4213f8550abc3
ambabbit <ambabbit@yahoo.com>
2019-11-10 16:14:40 -0500

    fixed merge conflict

--- Scripts.js
+++ Scripts.js
@@ -3,14 +3,15 @@
 var airportSchedule = [];
 airportSchedule = d3.csv("./Test_Data/SCHEDULES_ALL_ALL_DIR_01DEC2019_31DEC2019 Yes CodeShare.csv");
 
-
 async function FilterBasedOnAirportCode(s) {
-    const airSchedule = await airportSchedule;
-    const match = [];
+    const airSchedule = await airportSchedule,
+    match = [];
+
     let offset = 0;
-    console.log(typeof(airSchedule));
+   
     const results = airSchedule.filter(airport => airport["Origin Airport"] == s || airport["Destination Airport"] == s)
-    results.forEach(function (d, i){
+   
+    results.forEach(function (d){
         match[offset++] = {
             Origin_Airport: d["Origin Airport"],
             Destination_Airport: d["Destination Airport"],
